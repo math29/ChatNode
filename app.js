4798e663e50efe8be44a4cf1bb73df31bc597866
math29 <maaath29@gmail.com>
2015-04-18 07:25:11 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.get('/', function(req, res) {
+    res.render('chat.ejs');
+});
+
+// Quand on client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket, pseudo) {
+    console.log('Un client est connecté !');
+
+    socket.on('new_user', function(pseudo){
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('new_user', pseudo);
+    });
+
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    }); 
+});
+
+server.listen(8080);
